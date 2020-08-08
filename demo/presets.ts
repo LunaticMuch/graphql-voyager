7f35ddcff7a989afa5cf0d2a3d40c2d800c7a148
Stefano Cislaghi <stefano.cislaghi@aexp.com>
2020-08-08 12:42:46 +0100

    added twitter load

--- demo/presets.ts
+++ demo/presets.ts
@@ -3,11 +3,15 @@ const swapiIntrospection = require('./presets/swapi_introspection.json');
 const yelpIntrospection = require('./presets/yelp_introspection.json');
 const shopifyIntrospection = require('./presets/shopify_introspection.json');
 
+import { buildSchema, introspectionFromSchema } from 'graphql/utilities';
+const twitterIntrospection= {data: introspectionFromSchema(buildSchema('./presets/twitter.graphql'))};
+
 export const PRESETS = {
   'Star Wars': swapiIntrospection,
   Yelp: yelpIntrospection,
   'Shopify Storefront': shopifyIntrospection,
   GitHub: githubIntrospection,
+  Twitter: twitterIntrospection
 };
 
 export const defaultPresetName = 'Star Wars';
